f706035748df0b36f887a75f02c72282f530c589
Crowdbotics <team@crowdbotics.com>
2023-07-17 04:04:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "manic-managed-42225",
+  url: "https://manic-managed-42225.botics.co",
+  api: "https://manic-managed-42225.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
